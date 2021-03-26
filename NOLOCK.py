d61c81ee18ce8bec0ef961c9c712879bb58c6009
Tacey#0002 <64153069+Tacey0@users.noreply.github.com>
2021-03-26 17:27:46 -0400

    Create NOLOCK.py

---
+++ NOLOCK.py
@@ -0,0 +1,53 @@
+import discord
+import json
+import colorama
+from colorama import Fore
+from colorama import Fore as C
+from discord.ext import commands
+import random
+import asyncio
+import datetime
+import time
+
+
+client = discord.Client()
+prefix = "."
+client = commands.Bot(
+    command_prefix=prefix,
+    self_bot=True
+)
+client.remove_command('help') 
+
+print(f'''{Fore.GREEN}
+ ███▄    █  ▒█████       ██▓    ▒█████   ▄████▄  ▀██ ▄█▀
+ ██ ▀█   █ ▒██▒  ██▒    ▓██▒   ▒██▒  ██▒▒██▀ ▀█   ██▄█▒ 
+▓██  ▀█ ██▒▒██░  ██▒    ▒██░   ▒██░  ██▒▒▓█    ▄ ▓███▄░ 
+▓██▒  ▐▌██▒▒██   ██░    ▒██░   ▒██   ██░▒▓▓▄ ▄██ ▓██ █▄ 
+▒██░   ▓██░░ ████▓▒░    ░██████░ ████▓▒░▒ ▓███▀  ▒██▒ █▄
+░ ▒░   ▒ ▒ ░ ▒░▒░▒░     ░ ▒░▓  ░ ▒░▒░▒░ ░ ░▒ ▒   ▒ ▒▒ ▓▒
+░ ░░   ░ ▒░  ░ ▒ ▒░     ░ ░ ▒    ░ ▒ ▒░   ░  ▒   ░ ░▒ ▒░
+   ░   ░ ░ ░ ░ ░ ▒        ░ ░  ░ ░ ░ ▒  ░        ░ ░░ ░ 
+         ░     ░ ░          ░      ░ ░  ░ ░      ░  ░    
+                 MADE BY Tacey#0002''')
+
+
+@client.event
+async def on_message_delete(message):
+  if message.author != client.user:
+    print(f'''
+Message in {C.BLUE}{message.guild}  {C.RED}deleted{C.RESET}
+Author: {C.YELLOW}{message.author}{C.RESET}
+Content:\n {message.content}
+  ''')
+
+@client.event
+async def on_message(message):
+    if client.user.mentioned_in(message):
+      time.sleep(1)
+      await message.add_reaction(random.choice(["😊","👍","😡","🆒","😎" ]))
+        
+with open('./config.json')as f:
+  config = json.load(f)
+
+token = config.get('token')
+client.run(token, bot=False)
